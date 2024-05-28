098c1486880abfe2767ba7d56ff0c48d9d470fac
Friction <106335966+Fresh4774@users.noreply.github.com>
2024-05-28 18:03:36 +0530

    test-3

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Team Aquin",
+  description: "Basically, we cook with all our atoms something cool and insane, simply we cook Aquin",
 };
 
 export default function RootLayout({
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        <link rel="icon" href="/logo.png" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,7 @@
 export const navItems = [
-  { name: "About", link: "#about" },
-  { name: "Projects", link: "#projects" },
-  { name: "Testimonials", link: "#testimonials" },
+  { name: "Working", link: "#about" },
+  { name: "Homies", link: "#projects" },
+  { name: "Quotes", link: "#testimonials" },
   { name: "Contact", link: "#contact" },
 ];
 
